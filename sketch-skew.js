8caf6ab4dea405ea2c523ed3c11523c52eb4f57d
Pamela Gotti <pamela.gotti@gmail.com>
2024-08-26 16:06:10 +0200

    * main: Drawing random rectangles

--- sketch-skew.js
+++ sketch-skew.js
@@ -1,15 +1,16 @@
 const canvasSketch = require('canvas-sketch');
 const math = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [ 1000, 1000 ]
 };
 
 const getCoordinates = (width, height) => {
-  const x = width * 0.5;
-  const y = width * 0.5;
-  const w = width * 0.6;
-  const h = height * 0.1;
+  const x = random.range(0, width);
+  const y = random.range(0, height);
+  const w = random.range(200, 600);
+  const h = random.range(40, 200);
   return [x, y, w, h];
 }
 
@@ -57,23 +58,28 @@ const skewedRectangle = (context, w=600, h=200, degrees=-45) => {
 
 const sketch = () => {
 
-  let angle, x, y, w, h;
+  let x, y, w, h;
+
+  const numRect = 20;
+  const degrees = -30;
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    [x, y, w, h] = getCoordinates(width, height);
-  
-    context.save();
+    for (let i = 0; i < 20; i ++) {
+      [x, y, w, h] = getCoordinates(width, height);
+    
+      context.save();
 
-    context.translate(x, y);
-    context.strokeStyle = 'blue';
+      context.translate(x, y);
+      context.strokeStyle = 'blue';
 
-    skewedRectangle(context);
+      skewedRectangle(context, w, h, degrees);
 
-    context.restore();
-    context.stroke();
+      context.stroke();
+      context.restore();
+    }
   };
 };
 
