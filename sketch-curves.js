fb0387ec5b237c4ac9152a9e1e5a5940e1d51fe6
Pamela Gotti <pamela.gotti@gmail.com>
2024-08-27 12:17:20 +0200

    * main: Added interaction

--- sketch-curves.js
+++ sketch-curves.js
@@ -1,14 +1,21 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true,
 };
 
-const sketch = () => {
+let elCanvas;
 
-  const points = [
+let points;
+
+const sketch = ({canvas}) => {
+
+  canvas.addEventListener('mousedown', onMouseDown);
+  elCanvas = canvas;
+  points = [
     new Point({ x: 200, y: 540 }),
-    new Point({ x: 400, y: 300, control: true }),
+    new Point({ x: 400, y: 1000, control: true }),
     new Point({ x: 880, y: 540 }),
   ];
 
@@ -24,13 +31,16 @@ const sketch = () => {
     context.stroke();
 
     points.forEach(point => point.draw(context));
-    
+
   };
 };
 
 canvasSketch(sketch, settings);
 
 class Point {
+
+  size = 10;
+
   constructor({x, y, control = false}){
     this.x = x;
     this.y = y;
@@ -45,9 +55,42 @@ class Point {
 
     context.beginPath();
     context.moveTo(this.x, this.y);
-    context.arc(0, 0, 10, 0, Math.PI * 2);
+    context.arc(0, 0, this.size, 0, Math.PI * 2);
     context.fill();
     
     context.restore();
   }
+
+  hitTest(x, y) {
+    return Math.hypot(this.x - x, this.y - y) < this.size;
+  }
 };
+
+const onMouseDown = (event) => {
+  window.addEventListener('mousemove', onMouseMove);
+  window.addEventListener('mouseup', onMouseUp);
+
+  const x = event.offsetX / elCanvas.offsetWidth * elCanvas.width;
+  const y = event.offsetY / elCanvas.offsetHeight * elCanvas.height;
+  
+  points.forEach(point => {
+    point.isDragging = point.hitTest(x, y);
+  });
+}
+
+const onMouseMove = (event) => {
+  const x = event.offsetX / elCanvas.offsetWidth * elCanvas.width;
+  const y = event.offsetY / elCanvas.offsetHeight * elCanvas.height;
+  
+  points.forEach(point => {
+    if (point.isDragging) {
+      point.x = x;
+      point.y = y;
+    }
+  });
+}
+
+const onMouseUp = () => {    
+  window.removeEventListener('mousemove', onMouseMove);
+  window.removeEventListener('mouseup', onMouseUp);
+}
