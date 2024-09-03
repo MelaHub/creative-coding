f6b25b8bdecdf5988602639a1a5cf841caa4202a
Pamela Gotti <pamela.gotti@gmail.com>
2024-09-03 12:38:00 +0200

    * main: Updated wepack conf

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   output: {
     filename: '[name]',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'scripts')
   },
   mode: 'development'
 };
