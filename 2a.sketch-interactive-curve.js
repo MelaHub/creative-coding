7dfe0d26be871650350f50f2e3283b0848e6cc9a
Pamela Gotti <pamela.gotti@gmail.com>
2024-09-03 12:33:00 +0200

    * main: Added selection in the menu and description in the index page

--- 2a.sketch-interactive-curve.js
+++ 2a.sketch-interactive-curve.js
@@ -3,6 +3,7 @@ const canvasSketch = require('canvas-sketch');
 const settings = {
   dimensions: [ 1080, 1080 ],
   animate: true,
+  canvas: document.getElementById('interactive-curve'),
 };
 
 let elCanvas;
@@ -128,4 +129,9 @@ const onMouseMove = (event) => {
 const onMouseUp = () => {    
   window.removeEventListener('mousemove', onMouseMove);
   window.removeEventListener('mouseup', onMouseUp);
+}
+
+
+if (settings.canvas) {
+  canvasSketch(sketch, settings);
 }
