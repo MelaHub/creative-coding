ed50e5d293a0ad06afaf47e328b81f61fabc4e7e
Pamela Gotti <pamela.gotti@gmail.com>
2024-09-06 19:00:36 +0200

    * astro2: Updated github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://melahub.github.io',
+  base: 'creative-coding',
+})
