a6c3a666fca7c8020260fc8b02378ecdd03ac3be
Pamela Gotti <pamela.gotti@gmail.com>
2024-09-08 16:53:28 +0200

    * obey: Saving cursor position

--- canvas-sketch/obey.js
+++ canvas-sketch/obey.js
@@ -8,8 +8,29 @@ const settings = {
   canvas: document.getElementById('obey'),
 };
 
+class PointFromCenter {
+  constructor(x, y, distance, distanceCoord) {
+    this.x = x;
+    this.y = y;
+    this.distance = distance;
+    this.distanceCoord = distanceCoord;
+  }
+}
+
+class Cursor {
+  constructor({x, y, center}) {
+    this.x = x;
+    this.y = y;
+    this.center = center;
+    this.distance = Math.sqrt((x - center.x) * (x - center.x) + (y - center.y) * (y - center.y));
+    this.distanceCoord = { x: x - center.x, y: y - center.y };
+  }  
+
+}
+
 let elCanvas;
-let cursor = { x: 9999, y: 9999 };
+let cursor = new Cursor({ x: 9999, y: 9999, center: { x: 0, y: 0 } });
+let points = [];
 
 const sketch = ({canvas, width, height}) => {
 
@@ -24,20 +45,18 @@ const sketch = ({canvas, width, height}) => {
   let evenColor = random.pick(risoColors).hex;
   let oddColor = random.pick(risoColors).hex;
 
-  console.log(bgColor, lineColor, evenColor, oddColor);
   const circles = [];
   for (let i = numCircles - 1; i >= 0; i--) {
     const radius = baseRadius + i * gapCircle;
     circles.push(new Circle(radius, width/2, height/2, lineColor, i % 2 === 0 ? evenColor : oddColor));
   }
-  cursor = { x: width/2, y: height/2 };
+  cursor = new Cursor({ x: width/2, y: height/2, center: { x: width/2, y: height/2 } });
 
   return ({ context, width, height }) => {
     context.fillStyle = bgColor;
     context.fillRect(0, 0, width, height);
 
     circles.forEach(circle => {
-      circle.update();
       circle.draw(context);
   })};
 };
@@ -46,21 +65,9 @@ class Circle {
   constructor(radius, centerX, centerY, lineColor, fillColor) {
     this.radius = radius;
     this.center = { x: centerX, y: centerY };
-    this.point = { x: cursor.x, y: cursor.y };
     this.maxDistance = 200;
     this.lineColor = lineColor;
     this.fillColor = fillColor;
-  }
-
-  update() { 
-    this.distanceCoord = { x: cursor.x - this.center.x, y: cursor.y - this.center.y };
-    this.distance = Math.sqrt(this.distanceCoord.x * this.distanceCoord.x + this.distanceCoord.y * this.distanceCoord.y);
-    this.isCursorInside = this.distance < this.radius;
-    if (this.distance > this.maxDistance + this.radius) {
-      this.point = { x: this.center.x - (this.maxDistance + this.radius) * (this.center.x - cursor.x) / this.distance, y: this.center.y - (this.maxDistance + this.radius) * (this.center.y - cursor.y) / this.distance };
-    } else {
-      this.point = { x: cursor.x, y: cursor.y };
-    }
   }
 
   draw(context) {
@@ -68,11 +75,14 @@ class Circle {
 
     context.strokeStyle = this.lineColor;
     context.lineWidth = 7;
-
+    points.forEach(point => {
+      this.drawOutsideCircle(context, point);
+    });
+    this.isCursorInside = cursor.distance < this.radius;
     if (this.isCursorInside) {
       this.drawInsideCircle(context);
     } else {
-      this.drawOutsideCircle(context);
+      this.drawOutsideCircle(context, cursor);
     }
 
     context.restore();
@@ -86,17 +96,22 @@ class Circle {
     context.stroke();
   }
 
-  drawOutsideCircle(context) {
-    const a = this.radius * this.radius / this.distance;
-    const x = this.center.x + a * this.distanceCoord.x / this.distance;
-    const y = this.center.y + a * this.distanceCoord.y / this.distance;
+  drawOutsideCircle(context, point) {
+    const a = this.radius * this.radius / point.distance;
+    const x = this.center.x + a * point.distanceCoord.x / point.distance;
+    const y = this.center.y + a * point.distanceCoord.y / point.distance;
     const p = Math.sqrt(this.radius * this.radius - a * a);
-    const tang1 = { x: x - p * this.distanceCoord.y / this.distance, y: y + p * this.distanceCoord.x / this.distance };
-    const tang2 = { x: x + p * this.distanceCoord.y / this.distance, y: y - p * this.distanceCoord.x / this.distance };
+    const tang1 = { x: x - p * point.distanceCoord.y / point.distance, y: y + p * point.distanceCoord.x / point.distance };
+    const tang2 = { x: x + p * point.distanceCoord.y / point.distance, y: y - p * point.distanceCoord.x / point.distance };
     context.beginPath();
     context.fillStyle = this.fillColor;
     context.moveTo(tang1.x, tang1.y);
-    context.lineTo(this.point.x, this.point.y);
+
+    let shapeTip = { x: point.x, y: point.y };
+    if (point.distance > this.maxDistance + this.radius) {
+      shapeTip = { x: this.center.x - (this.maxDistance + this.radius) * (this.center.x - point.x) / point.distance, y: this.center.y - (this.maxDistance + this.radius) * (this.center.y - point.y) / point.distance };
+    }
+    context.lineTo(shapeTip.x, shapeTip.y);
     context.lineTo(tang2.x, tang2.y);
     context.fill();
     context.stroke();
@@ -124,14 +139,13 @@ const onMouseMove = (event) => {
   const x = event.offsetX / elCanvas.offsetWidth * elCanvas.width;
   const y = event.offsetY / elCanvas.offsetHeight * elCanvas.height;
   
-  cursor.x = x;
-  cursor.y = y;
+  cursor = new Cursor({ x, y, center: { x: elCanvas.width/2, y: elCanvas.height/2 } });
 }
 
 onMouseUp = () => {
   window.removeEventListener('mousemove', onMouseMove);
   window.removeEventListener('mouseup', onMouseUp);
 
-  cursor.x = elCanvas.width/2;
-  cursor.y = elCanvas.height/2;
+  points.push(new PointFromCenter(cursor.x, cursor.y, cursor.distance, cursor.distanceCoord));
+
 }
