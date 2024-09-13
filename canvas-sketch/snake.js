44ed5d855a69c24c413d1b448760f0e5f0bd8d66
Pamela Gotti <pamela.gotti@gmail.com>
2024-09-13 15:10:45 +0200

    * snake: Basic structure

---
+++ canvas-sketch/snake.js
@@ -0,0 +1,122 @@
+const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+
+const settings = {
+  dimensions: [ 1080, 1080 ],
+  animate: true,
+  fps: 1,
+};
+
+const numSquares = 1;
+const blocksPerSquareSide = 9;
+const blockSize = 105;
+const squareSideSize = blockSize * blocksPerSquareSide;
+const blockLineWidth = 10;
+
+class Block {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.next = null;
+    this.previous = null;
+    this.partOfSnake = false;
+    this.freeColor = 'white';
+    this.snakeColor = 'red';
+    this.strokeStyle = 'lightgray';
+  }
+
+  draw(context) {
+    context.lineWidth = blockLineWidth;
+    context.strokeStyle = this.strokeStyle;
+    context.fillStyle = this.partOfSnake ? this.snakeColor : this.freeColor;
+    if (this.partOfSnake) {
+      context.beginPath();
+      context.moveTo(this.x * this.size, this.y * this.size);
+      context.lineTo(this.x * this.size + this.size, this.y * this.size);
+      context.lineTo(this.x * this.size + this.size, this.y * this.size + this.size);
+      context.lineTo(this.x * this.size, this.y * this.size + this.size);
+      context.closePath();
+      context.fill();
+      context.stroke();
+    }
+    context.moveTo(this.x * this.size, this.y * this.size);
+    
+  }
+}
+
+const blocks = [];
+const snakesStart = [];
+
+const sketch = ({context, width, height}) => {
+  context.fillStyle = 'white';
+  context.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < blocksPerSquareSide; i++) {
+    for (let j = 0; j < blocksPerSquareSide; j++) {
+      const block = new Block(i, j, blockSize);
+      blocks.push(block);
+    }
+  }
+
+  blocks[0].partOfSnake = true;
+  snakesStart.push(blocks[0]);
+
+  return ({ context, width, height, frame }) => {
+    context.translate(width / 2 - blocksPerSquareSide * blockSize / 2, height / 2 - blocksPerSquareSide * blockSize / 2);
+    updateSnakes(context);
+    console.log(frame);
+    blocks.forEach(block => block.draw(context));
+  };
+};
+
+const updateSnakes = (context) => {
+  const lastSnakeStart = snakesStart[snakesStart.length - 1];
+  let lastSnakeBlock = lastSnakeStart;
+
+  while (lastSnakeBlock.next) {
+    lastSnakeBlock = lastSnakeBlock.next;
+  }
+
+  let availableDirections = ['left', 'right', 'up', 'down'];
+  let nextBlock = null;
+  let isNextInsideSquare = false;
+  let newBlockIndex = null;
+  while (availableDirections.length > 0 && !nextBlock) {
+    const nextDir = random.pick(availableDirections);
+    newBlockIndex = lastSnakeBlock.x * blocksPerSquareSide + lastSnakeBlock.y;
+    switch (nextDir) {
+      case 'left':
+        isNextInsideSquare = lastSnakeBlock.x > 0;
+        nextBlockIdx = newBlockIndex - 1;
+        break;
+      case 'right':
+        isNextInsideSquare = lastSnakeBlock.x < blocksPerSquareSide - 1;
+        nextBlockIdx = newBlockIndex + 1;
+        break;
+      case 'up':
+        isNextInsideSquare = lastSnakeBlock.y > 0;
+        nextBlockIdx = newBlockIndex - blocksPerSquareSide;
+        break;
+      case 'down':
+        isNextInsideSquare = lastSnakeBlock.y < blocksPerSquareSide - 1;
+        nextBlockIdx = newBlockIndex + blocksPerSquareSide;
+        break;
+    }
+    if (isNextInsideSquare) {
+      const maybeNextBlock = blocks[nextBlockIdx];
+      if (maybeNextBlock && !maybeNextBlock.partOfSnake) {
+        nextBlock = maybeNextBlock;
+      }
+    }
+    availableDirections.pop(nextDir);
+  }
+
+  if (nextBlock) {
+    lastSnakeBlock.next = nextBlock;
+    nextBlock.previous = lastSnakeBlock;
+    nextBlock.partOfSnake = true;
+  }
+};
+
+canvasSketch(sketch, settings);
