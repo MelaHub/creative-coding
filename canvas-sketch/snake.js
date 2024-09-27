8f822a90dcaf61e7564a62f2bdc7164f2128f1c6
Pamela Gotti <pamela.gotti@gmail.com>
2024-09-27 10:52:52 +0200

    * snake: Added a bit of color

--- canvas-sketch/snake.js
+++ canvas-sketch/snake.js
@@ -1,60 +1,63 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const colormap = require('colormap');
 
 const settings = {
   dimensions: [ 1080, 1080 ],
   animate: true,
-  // fps: 1,
 };
 
 const squaresRows = 5;
 const squaresCols = 5;
-const blocksPerSquareSide = 5;
-const blockLineWidth = 10;
+const blocksPerSquareSide = 10;
+const blockLineWidth = 7;
 const speed = 1;
+
+const colors = colormap({
+  colormap: 'rainbow-soft',
+});
 
 let squares = [];
 
 class Block {
 
-  constructor(x, y, size, idx, arrivingFromDir) {
+  constructor(x, y, size, idx, arrivingFromDir, fillColor) {
     this.x = x;
     this.y = y;
     this.size = size;
     this.idx = idx;
     this.used = false;
     this.arrivingFromDir = arrivingFromDir;
+    this.fillColor = fillColor;
   }
 
   draw(context) {
+    context.beginPath();
     context.lineWidth = blockLineWidth;
-    context.strokeStyle = 'gray';
-    context.fillStyle = 'green';
-    if (!this.arrivingFromDir) {
-      context.fillStyle = 'blue';
-    } 
+    context.strokeStyle = 'white';
+    context.fillStyle = this.fillColor;
     context.fillRect(this.x * this.size, this.y * this.size, this.size, this.size);    
     context.strokeRect(this.x * this.size, this.y * this.size, this.size, this.size);
-    context.strokeStyle = 'green';
+    context.strokeStyle = this.fillColor;
     switch(this.arrivingFromDir) {
       case 'right': 
-        context.moveTo(this.x * this.size, this.y * this.size);
-        context.lineTo(this.x * this.size, this.y * this.size + this.size);
+        context.moveTo(this.x * this.size, this.y * this.size + blockLineWidth / 2);
+        context.lineTo(this.x * this.size, this.y * this.size + this.size - blockLineWidth / 2);
         break;
       case 'left': 
-        context.moveTo(this.x * this.size + this.size, this.y * this.size);
-        context.lineTo(this.x * this.size + this.size, this.y * this.size + this.size);
+        context.moveTo(this.x * this.size + this.size, this.y * this.size + blockLineWidth / 2);
+        context.lineTo(this.x * this.size + this.size, this.y * this.size + this.size - blockLineWidth / 2);
         break;
       case 'down':
-        context.moveTo(this.x * this.size, this.y * this.size);
-        context.lineTo(this.x * this.size + this.size, this.y * this.size);
+        context.moveTo(this.x * this.size + blockLineWidth / 2, this.y * this.size);
+        context.lineTo(this.x * this.size + this.size - blockLineWidth / 2, this.y * this.size);
         break;
       case 'up':
-        context.moveTo(this.x * this.size, this.y * this.size + this.size);
-        context.lineTo(this.x * this.size + this.size, this.y * this.size + this.size);
+        context.moveTo(this.x * this.size + blockLineWidth / 2, this.y * this.size + this.size);
+        context.lineTo(this.x * this.size + this.size - blockLineWidth / 2, this.y * this.size + this.size);
         break;
     }
-    context.stroke()
+    context.stroke();
   }
 }
 
@@ -134,37 +137,44 @@ class Square {
 
     if (!this.movingFrom) {
       let availableBlocks = this.blocks.filter(block => !block.used);
-      this.movingFrom = random.pick(availableBlocks);
-      this.movingFrom.used = true;
-      this.movingFrom.isHead = true;
-    }
-    if (!this.movingTo) {
-      this.pickNextBlock();
-      this.currentBlock = new Block(this.movingFrom.x, this.movingFrom.y, this.movingFrom.size, this.movingFrom.idx, this.movingFrom.arrivingFromDir, this.movingFrom.isHead);
-    } else {
-      switch (this.currDir) {
-        case 'left':
-          this.currentBlock = new Block(this.currentBlock.x - speed, this.currentBlock.y, this.currentBlock.size, this.movingFrom.idx, this.movingFrom.arrivingFromDir);
-          break;
-        case 'right':
-          this.currentBlock = new Block(this.currentBlock.x + speed, this.currentBlock.y, this.currentBlock.size, this.movingFrom.idx, this.movingFrom.arrivingFromDir);
-          break;
-        case 'up':
-          this.currentBlock = new Block(this.currentBlock.x, this.currentBlock.y - speed, this.currentBlock.size, this.movingFrom.idx, this.movingFrom.arrivingFromDir);
-          break;
-        case 'down':
-          this.currentBlock = new Block(this.currentBlock.x, this.currentBlock.y + speed, this.currentBlock.size, this.movingFrom.idx, this.movingFrom.arrivingFromDir);
-          break;
+      if (availableBlocks.length > 0) {
+        this.movingFrom = random.pick(availableBlocks);
+        let color = random.pick(colors);
+        this.movingFrom.used = true;
+        this.movingFrom.fillColor = color;
       }
     }
-    this.currentBlock.draw(context);
-    if (this.movingTo) {
-      console.log("Moving from", this.movingFrom.x, this.movingFrom.y, "to", this.movingTo.x, this.movingTo.y, "via", this.currentBlock.x, this.currentBlock.y, "dir", this.currDir);
-    }
-    if (this.movingTo && Math.abs(this.currentBlock.x - this.movingTo.x) <= speed && Math.abs(this.currentBlock.y - this.movingTo.y) <= speed) {
-      this.movingFrom = new Block(this.movingTo.x, this.movingTo.y, this.movingTo.size, this.movingTo.idx, this.movingTo.arrivingFromDir);
-      this.movingTo.used = true;
-      this.movingTo = null;
+    if (this.movingFrom) {
+      if (!this.movingTo) {
+        this.pickNextBlock();
+        this.currentBlock = new Block(this.movingFrom.x, this.movingFrom.y, this.movingFrom.size, this.movingFrom.idx, this.movingFrom.arrivingFromDir, this.movingFrom.fillColor);
+      } else {
+        switch (this.currDir) {
+          case 'left':
+            this.currentBlock = new Block(this.currentBlock.x - speed, this.currentBlock.y, this.currentBlock.size, this.movingFrom.idx, this.movingFrom.arrivingFromDir);
+            break;
+          case 'right':
+            this.currentBlock = new Block(this.currentBlock.x + speed, this.currentBlock.y, this.currentBlock.size, this.movingFrom.idx, this.movingFrom.arrivingFromDir);
+            break;
+          case 'up':
+            this.currentBlock = new Block(this.currentBlock.x, this.currentBlock.y - speed, this.currentBlock.size, this.movingFrom.idx, this.movingFrom.arrivingFromDir);
+            break;
+          case 'down':
+            this.currentBlock = new Block(this.currentBlock.x, this.currentBlock.y + speed, this.currentBlock.size, this.movingFrom.idx, this.movingFrom.arrivingFromDir);
+            break;
+        }
+      }
+      this.currentBlock.draw(context);
+      if (this.movingTo) {
+        console.log("Moving from", this.movingFrom.x, this.movingFrom.y, "to", this.movingTo.x, this.movingTo.y, "via", this.currentBlock.x, this.currentBlock.y, "dir", this.currDir);
+      } else {
+        this.movingFrom = null;
+      }
+      if (this.movingTo && Math.abs(this.currentBlock.x - this.movingTo.x) <= speed && Math.abs(this.currentBlock.y - this.movingTo.y) <= speed) {
+        this.movingFrom = new Block(this.movingTo.x, this.movingTo.y, this.movingTo.size, this.movingTo.idx, this.movingTo.arrivingFromDir, this.movingFrom.fillColor);
+        this.movingTo.used = true;
+        this.movingTo = null;
+      }
     }
     context.restore();
   }
@@ -194,309 +204,3 @@ const sketch = ({context, width, height}) => {
 }
 
 canvasSketch(sketch, settings);
-
-  /* for (let i = 0; i < blocksPerSquareSide; i++) {
-    for (let j = 0; j < blocksPerSquareSide; j++) {
-      const block = new Block(i, j, blockSize);
-      blocks.push(block);
-    }
-  }
-
-  blocks[0].partOfSnake = true;
-  snakesStart.push(blocks[0]);
-
-  return ({ context, width, height, frame }) => {
-    context.translate(width / 2 - blocksPerSquareSide * blockSize / 2, height / 2 - blocksPerSquareSide * blockSize / 2);
-    updateSnakes(context);
-    context.save();
-    blocks.forEach(block => {
-      console.log(block.x, block.y);
-      block.draw(context)
-    });
-    context.restore();
-  };
-};
-
-
-/* class Square {
-  constructor(x, y, size) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.blocks = [];
-    this.movingFrom = null;
-    this.movingTo = null;
-    this.currentBlock = null;
-    this.currDir = null;
-    this.speed = 0.1;
-
-    for (let i = 0; i < blocksPerSquareSide; i++) {
-      for (let j = 0; j < blocksPerSquareSide; j++) {
-        let block = new Block(i, j, blockSize);
-        this.blocks.push(block);
-      }
-    }
-  }
-
-  update(context) {
-    let nextDir = null;
-    if (!this.movingFrom) {
-      this.movingFrom = random.pick(this.blocks);
-      this.movingFrom.partOfSnake = true;
-      this.currentBlock = new Block(this.movingFrom.x, this.movingFrom.y, blockSize);
-    } else if (!this.movingTo) {
-      let availableDirections = ['left', 'right', 'up', 'down'];
-      let isNextInsideSquare = false;
-      let newBlockIndex = null;
-      let nextBlockIdx = null;
-      while (availableDirections.length > 0 && !this.movingTo) {
-        nextDir = random.pick(availableDirections);
-        newBlockIndex = this.movingFrom.x * blocksPerSquareSide + this.movingFrom.y;
-        switch (nextDir) {
-          case 'left':
-            isNextInsideSquare = this.movingFrom.x > 0;
-            nextBlockIdx = newBlockIndex - 1;
-            break;
-          case 'right':
-            isNextInsideSquare = this.movingFrom.x < blocksPerSquareSide - 1;
-            nextBlockIdx = newBlockIndex + 1;
-            break;
-          case 'up':
-            isNextInsideSquare = this.movingFrom.y > 0;
-            nextBlockIdx = newBlockIndex - blocksPerSquareSide;
-            break;
-          case 'down':
-            isNextInsideSquare = this.movingFrom.y < blocksPerSquareSide - 1;
-            nextBlockIdx = newBlockIndex + blocksPerSquareSide;
-            break;
-        }
-        if (isNextInsideSquare) {
-          const maybeNextBlock = this.blocks[nextBlockIdx];
-          if (maybeNextBlock && !maybeNextBlock.partOfSnake) {
-            this.movingTo = maybeNextBlock;
-          }
-        }
-        availableDirections.pop(nextDir);
-      }
-
-      if (this.movingTo) {
-        this.movingFrom.next = this.movingTo;
-        this.movingTo.previous = this.movingFrom;
-        this.movingTo.partOfSnake = true;
-        this.currDir = nextDir;
-      }
-    }
-    if (this.movingTo) {
-      switch (this.currDir) {
-        case 'left':
-          this.currentBlock.x -= this.speed;
-          break;
-        case 'right':
-          this.currentBlock.x += this.speed;
-          break;
-        case 'up':
-          this.currentBlock.y -= this.speed;
-          break;
-        case 'down':
-          this.currentBlock.y += this.speed;
-          break;
-      }
-      if (this.currentBlock.x == this.movingTo.x && this.currentBlock.y == this.movingTo.y) {
-        this.movingFrom = this.movingTo;
-        this.movingTo = null;
-      }
-    }
-    console.log(this.currDir, 'moving from', this.movingFrom.x, this.movingFrom.y, ' to ', this.movingTo.x, this.movingTo.y, ' via ', this.currentBlock.x, this.currentBlock.y);
-    this.currentBlock.draw(context);
-  }
-
-  init(context) {
-    context.moveTo(this.x, this.y); 
-    context.fillStyle = 'white';
-    context.fillRect(this.x, this.y, this.size, this.size);
-  }
-}
-
-class Block {
-  constructor(x, y, size) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.previous = null;
-    this.next = null;
-    this.partOfSnake = false;
-    this.freeColor = 'white';
-    this.snakeColor = 'red';
-    this.strokeStyle = 'lightgray';
-  }
-
-  otherNodeIsOnTheLeft(that) {
-    return that && 
-      that.x == this.x - 1 && 
-      that.y == this.y;
-  }
-
-  drawLeftSide(context) {
-    if (!this.otherNodeIsOnTheLeft(this.previous) && !this.otherNodeIsOnTheLeft(this.next)) { 
-      context.strokeStyle = this.strokeStyle;
-    } else {
-      context.strokeStyle = this.snakeColor;
-    }
-    context.moveTo(this.x * this.size, this.y * this.size);
-    context.lineTo(this.x * this.size, this.y * this.size + this.size);
-    context.stroke();
-  }
-
-  otherNodeIsOnTheRight(that) {
-    return that && 
-      that.x == this.x + 1 && 
-      that.y == this.y;
-  }
-
-  drawRightSide(context) {
-    if (!this.otherNodeIsOnTheRight(this.previous) && !this.otherNodeIsOnTheRight(this.next)) { 
-      context.strokeStyle = this.strokeStyle;
-    } else {
-      context.strokeStyle = this.snakeColor;
-    }
-    context.moveTo(this.x * this.size + this.size, this.y * this.size);
-    context.lineTo(this.x * this.size + this.size, this.y * this.size + this.size);
-    context.stroke();
-  }
-
-  otherNodeIsAbove(that) {
-    return that && 
-      that.x == this.x && 
-      that.y == this.y - 1;
-  }
-
-  drawUpperSide(context) {
-    if (!this.otherNodeIsAbove(this.previous) && !this.otherNodeIsAbove(this.next)) { 
-      context.strokeStyle = this.strokeStyle;
-    } else {
-      context.strokeStyle = this.snakeColor;
-    }
-    context.moveTo(this.x * this.size, this.y * this.size);
-    context.lineTo(this.x * this.size + this.size, this.y * this.size);
-    context.stroke();
-  }
-
-  otherNodeIsBelow(that) {
-    return that && 
-      that.x == this.x && 
-      that.y == this.y + 1;
-  }
-
-  drawBottomSide(context) {
-    if (!this.otherNodeIsBelow(this.previous) && !this.otherNodeIsBelow(this.next)) { 
-      context.strokeStyle = this.strokeStyle;
-    } else {
-      context.strokeStyle = this.snakeColor;
-    }
-    context.moveTo(this.x * this.size, this.y * this.size + this.size);
-    context.lineTo(this.x * this.size + this.size, this.y * this.size + this.size);
-    context.stroke();
-  }
-
-
-  draw(context) {
-    context.lineWidth = blockLineWidth;
-    context.strokeStyle = this.strokeStyle;
-    context.fillStyle = this.partOfSnake ? this.snakeColor : this.freeColor;
-    context.beginPath();
-    this.drawLeftSide(context);
-    this.drawRightSide(context);
-    this.drawUpperSide(context);
-    this.drawBottomSide(context);
-    context.fill();
-    context.stroke();
-  }
-}
-
-const blocks = [];
-const snakesStart = [];
-
-const sketch = ({context, width, height}) => {
-  const squares = new Square(0, 0, width);
-  squares.init(context);
-
-  return ({ context }) => {
-    // context.translate(width / 2 - blocksPerSquareSide * blockSize / 2, height / 2 - blocksPerSquareSide * blockSize / 2);
-    // context.save();
-    squares.update(context);
-    // context.restore();
-  };
-
-  /* for (let i = 0; i < blocksPerSquareSide; i++) {
-    for (let j = 0; j < blocksPerSquareSide; j++) {
-      const block = new Block(i, j, blockSize);
-      blocks.push(block);
-    }
-  }
-
-  blocks[0].partOfSnake = true;
-  snakesStart.push(blocks[0]);
-
-  return ({ context, width, height, frame }) => {
-    context.translate(width / 2 - blocksPerSquareSide * blockSize / 2, height / 2 - blocksPerSquareSide * blockSize / 2);
-    updateSnakes(context);
-    context.save();
-    blocks.forEach(block => {
-      console.log(block.x, block.y);
-      block.draw(context)
-    });
-    context.restore();
-  };
-};
-
-const updateSnakes = (context) => {
-  const lastSnakeStart = snakesStart[snakesStart.length - 1];
-  let lastSnakeBlock = lastSnakeStart;
-
-  while (lastSnakeBlock.next) {
-    lastSnakeBlock = lastSnakeBlock.next;
-  }
-
-  let availableDirections = ['left', 'right', 'up', 'down'];
-  let nextBlock = null;
-  let isNextInsideSquare = false;
-  let newBlockIndex = null;
-  while (availableDirections.length > 0 && !nextBlock) {
-    const nextDir = random.pick(availableDirections);
-    newBlockIndex = lastSnakeBlock.x * blocksPerSquareSide + lastSnakeBlock.y;
-    switch (nextDir) {
-      case 'left':
-        isNextInsideSquare = lastSnakeBlock.x > 0;
-        nextBlockIdx = newBlockIndex - 1;
-        break;
-      case 'right':
-        isNextInsideSquare = lastSnakeBlock.x < blocksPerSquareSide - 1;
-        nextBlockIdx = newBlockIndex + 1;
-        break;
-      case 'up':
-        isNextInsideSquare = lastSnakeBlock.y > 0;
-        nextBlockIdx = newBlockIndex - blocksPerSquareSide;
-        break;
-      case 'down':
-        isNextInsideSquare = lastSnakeBlock.y < blocksPerSquareSide - 1;
-        nextBlockIdx = newBlockIndex + blocksPerSquareSide;
-        break;
-    }
-    if (isNextInsideSquare) {
-      const maybeNextBlock = blocks[nextBlockIdx];
-      if (maybeNextBlock && !maybeNextBlock.partOfSnake) {
-        nextBlock = maybeNextBlock;
-      }
-    }
-    availableDirections.pop(nextDir);
-  }
-
-  if (nextBlock) {
-    lastSnakeBlock.next = nextBlock;
-    nextBlock.previous = lastSnakeBlock;
-    nextBlock.partOfSnake = true;
-  }
-};
-
-canvasSketch(sketch, settings);
-*/
